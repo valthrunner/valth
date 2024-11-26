bf287eec977ba2166a13272e29abe579a197cc12
Valthrunner <131185036+valthrunner@users.noreply.github.com>
2024-11-26 12:57:21 +0100

    Update run.bat

--- run.bat
+++ run.bat
@@ -4,15 +4,20 @@ setlocal EnableDelayedExpansion
 :: Check for administrative privileges and request if necessary
 net session >nul 2>&1
 if %errorlevel% neq 0 (
-    echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && ""%~s0"" %params%", "", "runas", 1 > "%temp%\getadmin.vbs"
+    echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && ""%~s0"" %*", "", "runas", 1 > "%temp%\getadmin.vbs"
     "%temp%\getadmin.vbs"
     del "%temp%\getadmin.vbs"
     exit /B
 )
 
+:: Determine the argument to pass
+set "ARG=%~1"
+if "%ARG%"=="" set "ARG=run"
+
 :: Set execution policy and download PowerShell script
 powershell -Command "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/valthrunner/valth/main/loader.ps1' -OutFile '%temp%\loader.ps1'"
 
-:: Execute PowerShell script
-powershell -ExecutionPolicy Bypass -File "%temp%\loader.ps1"
+:: Execute PowerShell script with the appropriate argument
+powershell -ExecutionPolicy Bypass -File "%temp%\loader.ps1" "%ARG%"
+
 exit /b
